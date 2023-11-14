ffb8b72d12bbabe2fc239e213fc9612c001c53c5
Nathan Prisbrey <ntpbanana@gmail.com>
2023-11-14 00:27:19 -0700

    Remove "type" Shadow Variable And Make TestLearningModels More Object Oriented

--- scripts/TestDataset.py
+++ scripts/TestDataset.py
@@ -9,7 +9,7 @@ from load_data import get_data_tensor, get_train_test_datasets
 
 
 class TestLearningModels:
-    def __init__(self, type: str, seq_len: int):
+    def __init__(self, seq_len: int=100):
         self.master_tensor = get_data_tensor(relative_change=False)
         self.seq_len = seq_len
         print(f"Master Tensor Shape: {self.master_tensor.shape}")
@@ -17,17 +17,6 @@ class TestLearningModels:
         print(f"Number of Training Examples: {len(self.master_train)}")
         print(f"Number of Testing Examples: {len(self.master_test)}")
         print("GPU available?: ", torch.cuda.is_available())
-
-        if type == "lstm":
-            self.lstm()
-        elif type == "rnn":
-            self.rnn()
-        elif type == "trans":
-            self.transformer()
-        elif type == "perc":
-            self.perceptron()
-        else:
-            print("Incorrect argument used. Try either \"lstm\", \"rnn\", \"trans\", or \"perc\".")
 
     def get_params(self, data_tensor: torch.Tensor):
         return (data_tensor[:,1:]), (data_tensor[:,0])
@@ -42,7 +31,7 @@ class TestLearningModels:
         print(y)
         return y
 
-    def perceptron(self):
+    def train_perceptron(self):
         clf = Perceptron()
         X,y = self.get_params(self.master_train)
         X_test,y_test = self.get_params(self.master_test)
@@ -51,21 +40,21 @@ class TestLearningModels:
         print(clf.n_iter_)
         print("Average days increasing in value: ", clf.predict(X_test).mean())
 
-    def mlp(self):
+    def train_RNN(self):
         clf = nn.RNN(input_size=246, hidden_size=1, num_layers=13010)
         X, y = self.get_params(self.master_train)
         out, hn = clf(X, y)
 
-    def lstm(self):
+    def train_LSTM(self):
         clf = nn.LSTM(input_size=246, hidden_size=1, num_layers=13010)
         X, y = self.get_params(self.master_train)
         out, hn = clf(X, y)
 
-    def transformer(self):
+    def train_transformer(self):
         clf = nn.Transformer()
         X, y = self.get_params(self.master_train)
         out, hn = clf(X, y)
         pass
 
 
-TestLearningModels("perc", 10000)
+TestLearningModels(seq_len=100)
