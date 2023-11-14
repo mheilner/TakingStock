77ece757d4872ecffe6932d14b9a6caed4340bb8
Nathan Prisbrey <ntpbanana@gmail.com>
2023-11-14 00:35:24 -0700

    Refactor TestLearningModels __init__ Method

--- scripts/TestDataset.py
+++ scripts/TestDataset.py
@@ -10,13 +10,20 @@ from load_data import get_data_tensor, get_train_test_datasets
 
 class TestLearningModels:
     def __init__(self, seq_len: int=100):
-        self.master_tensor = get_data_tensor(relative_change=False)
         self.seq_len = seq_len
-        print(f"Master Tensor Shape: {self.master_tensor.shape}")
-        self.master_train, self.master_test = get_train_test_datasets(self.master_tensor, self.seq_len, 0.8)
-        print(f"Number of Training Examples: {len(self.master_train)}")
-        print(f"Number of Testing Examples: {len(self.master_test)}")
-        print("GPU available?: ", torch.cuda.is_available())
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        master_tensor = get_data_tensor(relative_change=True)
+        print(f"Master Tensor Shape: {master_tensor.shape}")
+
+        self.train_dataset, self.test_dataset = get_train_test_datasets(
+                data_tensor=master_tensor,
+                seq_len=seq_len,
+                train_split=0.8)
+
+        print(f"Number of Training Examples: {len(self.train_dataset)}")
+        print(f"Number of Testing Examples: {len(self.test_dataset)}")
+        print(f"Training device: {self.device}")
 
     def get_params(self, data_tensor: torch.Tensor):
         return (data_tensor[:,1:]), (data_tensor[:,0])
@@ -42,17 +49,17 @@ class TestLearningModels:
 
     def train_RNN(self):
         clf = nn.RNN(input_size=246, hidden_size=1, num_layers=13010)
-        X, y = self.get_params(self.master_train)
+        X, y = self.get_params(self.train_dataset)
         out, hn = clf(X, y)
 
     def train_LSTM(self):
         clf = nn.LSTM(input_size=246, hidden_size=1, num_layers=13010)
-        X, y = self.get_params(self.master_train)
+        X, y = self.get_params(self.train_dataset)
         out, hn = clf(X, y)
 
     def train_transformer(self):
         clf = nn.Transformer()
-        X, y = self.get_params(self.master_train)
+        X, y = self.get_params(self.train_dataset)
         out, hn = clf(X, y)
         pass
 
